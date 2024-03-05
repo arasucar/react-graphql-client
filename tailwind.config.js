e35e4f65a404875d0a30c899fbd05dcdcf9b1e11
Aras <arasucar@icloud.com>
2024-03-05 18:33:32 +0100

    TailwindCSS is installed and configured.

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var"]
+      },
+      spacing: {
+        1: "4px",
+        2: "8px",
+        3: "12px",
+        4: "16px",
+        5: "20px",
+        6: "24px",
+        8: "32px",
+        10: "40px",
+        12: "48px",
+        16: "64px"
+      }
+    },
+    screens: {
+      "2xs": "160px",
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px"
+    }
+  },
+  plugins: []
+}
