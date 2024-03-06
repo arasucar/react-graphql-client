3f9c63aefee20cac696c396c12b5159432fd67e3
Aras <arasucar@icloud.com>
2024-03-06 16:42:09 +0100

    React Router is installed configured

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,14 +2,28 @@ import React from "react"
 import Header from "./Header"
 import Home from "./Home"
 import Footer from "./Footer"
+import { Outlet, Route, Routes } from "react-router"
+import { BrowserRouter } from "react-router-dom"
 
-const App: React.FC = () => {
+const BasicLayout: React.FC = () => {
   return (
     <div className="app" data-testid="app">
       <Header />
-      <Home />
+      <Outlet />
       <Footer />
     </div>
+  )
+}
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BasicLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-const Footer = () => {
+const Footer: React.FC = () => {
   return (
     <div className="footer" data-testid="footer">
       <div data-testid="footer-author">Aras Ucar @ 2024</div>
